58e1eacdbfca57582d2f9e80a158b21ea8624853
lanwah <fjzsxulianghua@sina.com>
2016-04-10 00:51:10 +0800

    TripleDES - 3DES加密解密类编码完成
    Signed-off-by: lanwah <fjzsxulianghua@sina.com>

---
+++ Lanwah.CSharp.NET/Lanwah.CSharp.NET.Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lanwah.CSharp.NET.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3DES加密解密调试
+            SecurityLib.DES.TripleDESTest.RunTest();
+            Console.ReadKey();
+        }
+    }
+}
